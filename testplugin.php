bce869d668a2bea12bf5ccf7212db59fe06065d8
Josh Finney <josh.finney@clicky.co.uk>
2023-01-20 08:17:16 +0000

    first commit

---
+++ testplugin.php
@@ -0,0 +1,12 @@
+<?php
+   /*
+   Plugin Name: Test Plugin
+   Plugin URI: https://josh-finney.co.uk
+   description: >-
+  a test plugin
+   Version: 1.0
+   Author: Josh Finney
+   Author URI: https://josh-finney.co.uk
+   License: GPL2
+   */
+?>
